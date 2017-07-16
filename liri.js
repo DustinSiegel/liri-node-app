3bc136f2cb5c9501e1f0db291b7a483f8a7e90c5
Dustin J Siegel <weenzombie@gmail.com>
2017-07-15 18:45:11 -0600

    tweets write to a text file too

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var twitterAuthenticate = require("./keys.js");
+var fs = require('file-system');
 var Twitter = require('twitter');
 var userInput = process.argv[2];
 var client = new Twitter({
@@ -12,20 +13,27 @@ var params = {screen_name: 'MntGoatsDilemma'};
 
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   
-  if (userInput === "my-tweets") {
+  	if (userInput === "my-tweets") {
 
-  	// console.log(tweets);
+  		// console.log(tweets);
   	
-  	for(i = 0; i < tweets.length; i++) {
+  		for(i = 0; i < tweets.length; i++) {
 
-  	console.log("------------------------------------------------------------")
-    console.log("Mountain Goat Says: " + tweets[i].text);
-    console.log("-")
-    console.log("This is when he said it: " + tweets[i].created_at);
-    console.log("------------------------------------------------------------")
-	}
-  }
+  			console.log("------------------------------------------------------------");
+    		console.log("Mountain Goat Says: " + tweets[i].text);
+    		console.log("-")
+    		console.log("This is when he said it: " + tweets[i].created_at);
+    		console.log("------------------------------------------------------------");
+    		
+    		fs.appendFile("log.txt", "Mountain Goat Says: " + tweets[i].text + "This is when he said it: " + tweets[i].created_at, function(err) {
+    			if (err) {
+      				return console.log(err);
+    			}
+  			});
+    	}
+    }
 });
+
 
 // if (process.argv[2] === "my-tweets") {
 // 	console.log(tweets);
