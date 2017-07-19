cbaa04b5d85d37cb2522d52b712eb81f924eea37
Dustin J Siegel <weenzombie@gmail.com>
2017-07-19 16:58:34 -0600

    spotify functions properly

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var twitterAuthenticate = require("./keys.js");
 var fs = require('file-system');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var omdbApi = require('omdb-client');
 
 //Twitter API Keys
 var client = new Twitter({
@@ -11,10 +12,13 @@ var client = new Twitter({
  	access_token_secret: twitterAuthenticate.twitterKeys.access_token_secret
 });
 
+//Spotify API Keys
 var spotify = new Spotify({
 	id: twitterAuthenticate.spotifyKeys.clientID,
 	secret: twitterAuthenticate.spotifyKeys.clientSecret
 });
+
+// var omdb = "http://www.omdbapi.com/?apikey=40e9cece&plot=full&";
 
 var params = {screen_name: 'MntGoatsDilemma'};
 var userInput = process.argv[2];
@@ -35,23 +39,20 @@ switch (userInput) {
 function tweetPullAndLog() {
 
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  
-	  	if (userInput === "my-tweets") {
 	  	
-	  		for(i = 0; i < tweets.length; i++) {
+	  	for(i = 0; i < tweets.length; i++) {
 
-	  			console.log("------------------------------------------------------------");
-	    		console.log("Mountain Goat Says: " + tweets[i].text);
-	    		console.log("-")
-	    		console.log("This is when he said it: " + tweets[i].created_at);
-	    		console.log("------------------------------------------------------------");
+	  		console.log("------------------------------------------------------------");
+	    	console.log("Mountain Goat Says: " + tweets[i].text);
+	    	console.log("-")
+	    	console.log("This is when he said it: " + tweets[i].created_at);
+	    	console.log("------------------------------------------------------------");
 	    		
-	    		fs.appendFile("log.txt", "---------------------------------------------------------- Mountain Goat Says: " + tweets[i].text + "--- This is when he said it: " + tweets[i].created_at, function(err) {
-	    			if (err) {
-	      				return console.log('Error occured: ' + err);
-	    			}
-	  			});
-	    	}
+	    	fs.appendFile("log.txt", "---------------------------------------------------------- Mountain Goat Says: " + tweets[i].text + "--- This is when he said it: " + tweets[i].created_at, function(err) {
+	    		if (err) {
+	      			return console.log('Error occured: ' + err);
+	    		}
+	  		});
 	    }
 	});
 };
@@ -59,10 +60,7 @@ function tweetPullAndLog() {
 function songPullAndLog() {
 	
 	spotify.search({ type: 'track', query: "'" + userInputTwo + "'" }, function(err, song) {
-  		
-  		if (err) {
-    		return console.log('Error occurred: ' + err);
-  		}
+
 	  	console.log("------------------------------------------------------------");
 		console.log("Band/Artist name: " + song.tracks.items[0].artists[0].name); //Band name
 		console.log("Album name: " + song.tracks.items[0].album.name);	//Album name
@@ -70,6 +68,24 @@ function songPullAndLog() {
 		console.log("URL link to this song: " + song.tracks.items[0].external_urls.spotify);  //Song URL
 		console.log("------------------------------------------------------------");
 
-		fs.appendFile("log.txt", "---------------------------------------------------------- Band/Artist name: " + song.tracks.items[0].artists[0].name + "--- Album name: " + song.tracks.items[0].album.name + "--- Song name: " + song.tracks.items[0].name + "--- URL link to this song: " + song.tracks.items[0].external_urls.spotify);
+		fs.appendFile("log.txt", "---------------------------------------------------------- Band/Artist name: " + song.tracks.items[0].artists[0].name + "--- Album name: " + song.tracks.items[0].album.name + "--- Song name: " + song.tracks.items[0].name + "--- URL link to this song: " + song.tracks.items[0].external_urls.spotify, function(err) {
+
+	  		if (err) {
+	    		return console.log('Error occurred: ' + err);
+	  		}
+		});
+	});
+};
+
+function moviePullAndLog() {
+
+	var params = {
+    title: 'Terminator'
+	}
+
+	omdbApi.get(params, function(err, data) {
+		if (err) {
+	    	return console.log('Error occurred: ' + err);
+	  	}
 	});
 };
